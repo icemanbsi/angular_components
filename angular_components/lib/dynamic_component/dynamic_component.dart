55a495aaddec96b1b4f4250218b1541f0896e13f
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-12-05 14:14:01 -0500

    Bump observable dependency

--- angular_components/lib/dynamic_component/dynamic_component.dart
+++ angular_components/lib/dynamic_component/dynamic_component.dart
@@ -112,25 +112,25 @@ class DynamicComponent implements OnDestroy, AfterChanges {
           _componentFactory, _viewContainerRef);
       _onLoadController.add(_childComponent);
       _updateChildComponent();
-    // } else if (_componentType != null) {
-    //   // TODO(google): Remove this code once componentType is no longer used.
-    //   Type loadType = _componentType;
-    //   _slowComponentLoader
-    //       .loadNextToLocation(loadType, _viewContainerRef)
-    //       .then((ComponentRef componentRef) {
-    //     if (loadType != _componentType) {
-    //       // During the load time, the component type has changed,
-    //       // and the type we just loaded is no longer valid.
-    //       componentRef.destroy();
-    //       return;
-    //     }
-    //     if (_childComponent != null) {
-    //       throw 'Attempting to overwrite a dynamic component';
-    //     }
-    //     _childComponent = componentRef;
-    //     _onLoadController.add(componentRef);
-    //     _updateChildComponent();
-    //   });
+      // } else if (_componentType != null) {
+      //   // TODO(google): Remove this code once componentType is no longer used.
+      //   Type loadType = _componentType;
+      //   _slowComponentLoader
+      //       .loadNextToLocation(loadType, _viewContainerRef)
+      //       .then((ComponentRef componentRef) {
+      //     if (loadType != _componentType) {
+      //       // During the load time, the component type has changed,
+      //       // and the type we just loaded is no longer valid.
+      //       componentRef.destroy();
+      //       return;
+      //     }
+      //     if (_childComponent != null) {
+      //       throw 'Attempting to overwrite a dynamic component';
+      //     }
+      //     _childComponent = componentRef;
+      //     _onLoadController.add(componentRef);
+      //     _updateChildComponent();
+      //   });
     }
   }
 

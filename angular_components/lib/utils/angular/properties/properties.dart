a1f600cefc53446feb5c5907987e6e78b518278f
thomashii <thomashii@dukefirehawk.com>
2021-12-25 18:02:01 -0500

    Migrate utils/angular/properties

--- angular_components/lib/utils/angular/properties/properties.dart
+++ angular_components/lib/utils/angular/properties/properties.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 /// Parses [strValue] into a [bool].
 ///
 /// Only the following values are acceptable as `strValue`:
@@ -13,7 +11,7 @@
 ///
 /// **NOTE**: If [strValue] is an empty string (''), it is always true. This is
 /// because when you declare something like:
-///     <material-button disabled></material-button>
+///     `<material-button disabled></material-button>`
 ///
 /// ... The value of "disabled" is ''.
 bool _parseBool(String strValue) {
@@ -33,26 +31,6 @@ bool _parseBool(String strValue) {
   }
 }
 
-/// A typed version of [getDynamic].
-///
-/// If [inputValue] is a [bool], returns it.
-/// If [inputValue] is a [String], then '' and 'true' evaluate to `true` while
-/// 'false' evaluates to `false`. All other string values produce an argument
-/// error.
-///
-/// **NOTE**: inputValue must be non-null.
-@Deprecated('Angular now supports boolean properties natively, for @Attribute'
-    ' use [attributeToBool].')
-bool getBool(inputValue) {
-  if (inputValue == null) throw ArgumentError.notNull('inputValue');
-
-  if (inputValue is String) return _parseBool(inputValue);
-  if (inputValue is bool) return inputValue;
-
-  throw ArgumentError.value(
-      inputValue, 'inputValue', 'Expected a String, or bool type');
-}
-
 /// Parses HTML attribute [String] to a [bool].
 ///
 /// Should be used to parse values passed to @Attribute constructor argument.
@@ -68,7 +46,7 @@ bool getBool(inputValue) {
 /// * <my-component foo> - foo attribute is present but has no value, which
 ///                        is parsed to *true*.
 /// * <my-component> - no attribute is present, parsed to [defaultValue].
-bool attributeToBool(String inputValue, {bool defaultValue = false}) {
+bool attributeToBool(String? inputValue, {bool defaultValue = false}) {
   if (inputValue == null) return defaultValue;
   return _parseBool(inputValue);
 }

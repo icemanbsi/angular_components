3110588f6319faac57a8b00f3fc733f1b216c69f
thomashii <thomashii@dukefirehawk.com>
2021-12-25 18:06:53 -0500

    Migrate utils/angular/reference

--- angular_components/lib/utils/angular/reference/reference.dart
+++ angular_components/lib/utils/angular/reference/reference.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
 import 'dart:html';
 
 import 'package:angular/angular.dart';
@@ -15,14 +14,16 @@ import 'package:angular/angular.dart';
 /// Place on an element or component to expose the [Element] as 'ref'.
 ///
 /// Example usage:
-///     <material-button ref #button="ref"></materialButton>
-///     <!-- You can now use 'button' in this template as a property -->
+/// ```html
+///   <material-button ref #button="ref"></materialButton>
+///   <!-- You can now use 'button' in this template as a property -->
+/// ```
 @Directive(
   selector: '[ref]',
   exportAs: 'ref',
 )
 class ReferenceDirective {
-  final Element element;
+  final Element? element;
 
-  ReferenceDirective(this.element);
+  ReferenceDirective(@Optional() this.element);
 }

a4d14cc5a0dc235c9e885a08490f4c1d11a532c7
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2022-01-03 22:11:04 -0500

    Add some tests for utils/browser/feature_detector
    Not complete yet, just making sure it runs.
    
    Signed-off-by: GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>

---
+++ angular_components/test/utils/browser/feature_detector/feature_detector_test.dart
@@ -0,0 +1,13 @@
+@TestOn('chrome')
+
+// import 'dart:html';
+import 'package:test/test.dart';
+
+import 'package:angular_components/utils/browser/feature_detector/feature_detector.dart';
+
+void main() {
+  test('', () {
+		expect(isFirefox, isFalse);
+		expect(isEdge, isFalse);
+	});
+}

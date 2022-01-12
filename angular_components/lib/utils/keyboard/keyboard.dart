c3a22750328879404e9004fb5f4202223238147c
Gavin Zhao <git@gzgz.dev>
2022-01-11 23:18:23 -0500

    Partially migrate utils/keyboard
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- angular_components/lib/utils/keyboard/keyboard.dart
+++ angular_components/lib/utils/keyboard/keyboard.dart
@@ -2,11 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:html';
 
-/// this utility method will check that keyboard even is happening
+/// This utility method will check that keyboard even is happening
 /// only with list of modifiers passed as true
 /// i.e. if called (modifiersKeysFor, shiftKey : true)
 /// it will return true if shift is pressed,

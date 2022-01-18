a05cbafa53bac9ecc861eabc74f67e6792c53157
Gavin Zhao <git@gzgz.dev>
2022-01-17 22:07:26 -0500

    Fully migrate utils/browser, testing needed
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- angular_components/lib/utils/browser/window/new_window_opener.dart
+++ angular_components/lib/utils/browser/window/new_window_opener.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:html';
 
 import 'package:angular/angular.dart';
@@ -22,7 +20,7 @@ void openInNewWindow(String url) {
   var anchorElement = AnchorElement(href: url)
     ..rel = 'noopener noreferrer'
     ..target = '_blank';
-  document.body.append(anchorElement);
+  document.body!.append(anchorElement);
   anchorElement.click();
   anchorElement.remove();
 }

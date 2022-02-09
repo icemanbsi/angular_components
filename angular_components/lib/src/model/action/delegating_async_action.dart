1e796988df225a30c798f0e3cb4125252c0e1def
Gavin Zhao <git@gzgz.dev>
2022-02-08 23:47:46 -0500

    Fully migrate src/model/action
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- angular_components/lib/src/model/action/delegating_async_action.dart
+++ angular_components/lib/src/model/action/delegating_async_action.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:async';
 
 import 'async_action.dart';
@@ -15,7 +13,7 @@ abstract class DelegatingAsyncAction<V> implements AsyncAction<V> {
   @override
   bool get cancelled => delegate.cancelled;
   @override
-  bool get isDone => delegate.isDone;
+  bool? get isDone => delegate.isDone;
   @override
   Future<V> get onDone => delegate.onDone;
   @override

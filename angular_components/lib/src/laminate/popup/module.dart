df74184e4541d571875292b84daf6ac46db4caac
Gavin Zhao <git@gzgz.dev>
2022-07-27 15:00:21 -0400

    fix(laminate): migrate more code to null safety
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- angular_components/lib/src/laminate/popup/module.dart
+++ angular_components/lib/src/laminate/popup/module.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'package:angular/angular.dart';
 import 'package:angular_components/src/laminate/enums/alignment.dart';
 import 'package:angular_components/src/laminate/overlay/module.dart';
